b7dcfbf2c153a36d275d5523961e51a7afd809ec
klarx94-Architect <klarx94@gmail.com>
2026-04-10 08:33:53 +0200

    feat(firebase): inicializar SDK con configuración del proyecto encapaco-menu

---
+++ src/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmNCdyuy7hLpiuQ2k1rPJUy-251GPGj8A",
+  authDomain: "encapaco-menu.firebaseapp.com",
+  projectId: "encapaco-menu",
+  storageBucket: "encapaco-menu.firebasestorage.app",
+  messagingSenderId: "1073419660674",
+  appId: "1:1073419660674:web:48b400b41d39bc7f13f39b"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
